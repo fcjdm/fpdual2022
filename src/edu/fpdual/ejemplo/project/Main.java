9359d4491e1f00db7901c023549a0fa0227ca898
fcjdm <fcjdom@gmail.com>
2022-04-07 10:07:32 +0200

    Primer commit

---
+++ src/edu/fpdual/ejemplo/project/Main.java
@@ -0,0 +1,12 @@
+package edu.fpdual.ejemplo.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int hola = 1;
+
+        hola = 2;
+        hola = 3;
+        System.out.println(args);
+    }
+}
